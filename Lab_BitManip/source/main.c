a632f1b60a130cf3bd980cf8f1ce6b231999d2a1
Hung Huynh <hhuyn019@wch136-28.cs.ucr.edu>
2019-10-07 12:34:46 -0700

    first

---
+++ Lab_BitManip/source/main.c
@@ -0,0 +1,30 @@
+/*	Author: hhuyn019
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0x00; PORTB = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	unsigned char tmpA = 0x00;
+	unsigned char tmpB = 0x00;
+	unsigned char tmpC = 0x00;
+	unsigned char count = 0x00;
+    /* Insert your solution below */
+    while (1) {
+	tmpA = PORTA;
+	tmpB = PORTB;
+	
+    return 1;
+}
