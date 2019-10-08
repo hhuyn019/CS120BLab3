b12cafb94328838a31127dda8dae20a6449affc3
Chris Huynh <hhuyn019@ucr.edu>
2019-10-08 02:12:29 -0700

    Finished part 1

--- Lab_BitManip/turnin/hhuyn019_lab3_part1.c
+++ Lab_BitManip/turnin/hhuyn019_lab3_part1.c
@@ -15,15 +15,34 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
-	//DDRB = 0x00; PORTB = 0xFF;
+	DDRB = 0x00; PORTB = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
 	unsigned char tmpA = 0x00;
-	//unsigned char tmpB = 0x00;
+	unsigned char tmpB = 0x00;
 	unsigned char tmpC = 0x00;
-	unsigned char fuel = 0x00;
-	unsigned char LED = 0x00;
+	unsigned char count = 0x00;
+	unsigned char tempCount = 0x00;
     /* Insert your solution below */
-	while (1) {
-		
+    while (1) {
+	tmpA = PINA;
+	tmpB = PINB;
+	for (unsigned char i = 0x00; i < 0x08; i = i + 0x01) {
+		tmpA = tmpA >> i;
+		tempCount = tmpA & 0x01;
+		if (tempCount == 0x01) {
+			count = count + 0x01;
+		}
 	}
+	i = 0x00;
+	for (unsigned char u = 0x00; u < 0x08; u = u + 0x01) {
+		tmpB = tmpB >> u;
+		tempCount = tmpB & 0x01;
+		if (tempCount == 0x01) {
+			count = count + 0x01;
+		}
+	}
+	u = 0x00;
+	PORTC = count;
+    }
+    return 1;
 }
