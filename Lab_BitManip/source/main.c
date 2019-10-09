a30bbc06d16e7beaa842308601a18b57d1437c1e
Hung Huynh <hhuyn019@bolt.cs.ucr.edu>
2019-10-08 22:50:21 -0700

    df

--- Lab_BitManip/source/main.c
+++ Lab_BitManip/source/main.c
@@ -15,15 +15,40 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
-	//DDRB = 0x00; PORTB = 0xFF;
+	DDRB = 0x00; PORTB = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
 	unsigned char tmpA = 0x00;
-	//unsigned char tmpB = 0x00;
-	unsigned char tmpC = 0x00;
-	unsigned char fuel = 0x00;
-	unsigned char LED = 0x00;
+	unsigned char tmpB = 0x00;
+	//unsigned char tmpC = 0x00;
+	unsigned char count = 0x00;
+	unsigned char tempCount = 0x00;
     /* Insert your solution below */
-	while (1) {
-		
+    while (1) {
+	tmpA = PINA;
+	tmpB = PINB;
+	//for (unsigned char i = 0x00; i < 0x08; i = i + 0x01) {
+	unsigned char i = 0x00;
+	while (i < 0x08) {
+		tmpA = tmpA >> i;
+		tempCount = tmpA & 0x01;
+		if (tempCount == 0x01) {
+			count = count + 0x01;
+		}
+		i = i + 0x01;
 	}
+	i = 0x00;
+	//for (unsigned char u = 0x00; u < 0x08; u = u + 0x01) {
+	unsigned char u = 0x00;
+	while (u < 0x08) {
+		tmpB = tmpB >> u;
+		tempCount = tmpB & 0x01;
+		if (tempCount == 0x01) {
+			count = count + 0x01;
+		}
+		u = u + 0x01;
+	}
+	u = 0x00;
+	PORTC = count;
+    }
+    return 1;
 }
